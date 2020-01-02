ec3b4c907b2f2788846dabdc9a0031b7d3430bcc
s11235218 <s11235218@163.com>
2020-01-02 17:13:29 +0800

    打印数组

---
+++ arrsysPrint/src/printTest.java
@@ -0,0 +1,14 @@
+
+public class printTest {
+    public static void main (String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        printArrays(arr);
+    }
+
+    public static void printArrays(int[] arr) {
+        for(int x: arr){
+            System. out. println(x);
+        }
+
+    }
+} 
