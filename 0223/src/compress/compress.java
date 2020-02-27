408868c11f237e5aad73cb8b5395a1cf15f72508
s11235218 <s11235218@163.com>
2020-02-27 21:22:54 +0800

    leetcode

--- 0223/src/compress/compress.java
+++ 0223/src/compress/compress.java
@@ -22,10 +22,26 @@ public class compress {
         return size;
     }
 
+    public static int compress2(char[] chars){
+        int anchor = 0, write = 0;
+        for (int read = 0; read < chars.length; read++) {
+            if (read + 1 == chars.length || chars[read + 1] != chars[read]) {
+                chars[write++] = chars[anchor];
+                if (read > anchor) {
+                    for (char c: ("" + (read - anchor + 1)).toCharArray()) {
+                        chars[write++] = c;
+                    }
+                }
+                anchor = read + 1;
+            }
+        }
+        return write;
+    }
+
     public static void main(String[] args) {
         char chars[] = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
 
-        System.out.println(compress(chars));
+        System.out.println(compress2(chars));
     }
 }
 
