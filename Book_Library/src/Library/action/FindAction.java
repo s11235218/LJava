a8a4797dd6a77302b328ce2e563c3bf696389607
s11235218 <s11235218@163.com>
2020-03-19 21:04:18 +0800

    控制台版本简易图书馆系统

--- Book_Library/src/Library/action/FindAction.java
+++ Book_Library/src/Library/action/FindAction.java
@@ -19,15 +19,4 @@ public class FindAction implements IAction {
             }
         }
     }
-//    public void work(BookList bookList){
-//        System.out.println("请输入书名：");
-//        Scanner scanner = new Scanner(System.in);
-//        String name = scanner.next();
-//        for (int i = 0; i < bookList.getSize(); i++) {
-//            Book book = bookList.getBook(i);
-//            if(book.getName().equals(name)){
-//                System.out.println(book);
-//            }
-//        }
-//    }
 }
