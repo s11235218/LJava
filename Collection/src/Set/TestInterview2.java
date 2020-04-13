97107e4d495fc3504a1b375de1ed5bb39cf0bd15
s11235218 <s11235218@163.com>
2020-04-13 21:57:54 +0800

    数据结构

--- Collection/src/Set/TestInterview2.java
+++ Collection/src/Set/TestInterview2.java
@@ -124,24 +124,24 @@ public class TestInterview2 {
     }
 
     public static void main(String[] args) {
-//        Map<Integer, Integer> map = new HashMap<>();
-//        map.put(1, 10);
-//        map.put(2, 20);
-//        map.put(3, 30);
-//        map.put(4, 40);
-//
-//        // 1. 使用迭代器
-//        // Entry 表示一个键值对 (jey, value)
-//        // entrySet 包含 Entry 的 Set Set<Entry<Integer, Integer>>
-//        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
-//        while (iterator.hasNext()) {
-//            Map.Entry<Integer, Integer> entry = iterator.next();
-//            System.out.println(entry.getKey() + ": " + entry.getValue());
-//        }
-//        // 2. 使用 for each
-//        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-//            System.out.println(entry.getKey() + ": " + entry.getValue());
-//        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 10);
+        map.put(2, 20);
+        map.put(3, 30);
+        map.put(4, 40);
+
+        // 1. 使用迭代器
+        // Entry 表示一个键值对 (jey, value)
+        // entrySet 包含 Entry 的 Set Set<Entry<Integer, Integer>>
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        // 2. 使用 for each
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
         int[] array = {9, 5, 2, 7};
         Arrays.sort(array);
