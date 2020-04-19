7a0084aa316bf915d2a5eb2537edf394581cee45
s11235218 <s11235218@163.com>
2020-04-19 10:26:26 +0800

    数据结构

--- Collection/src/reflect/TestReflect.java
+++ Collection/src/reflect/TestReflect.java
@@ -1,6 +1,6 @@
 package reflect;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.*;
 import java.util.Scanner;
 
 class Animal {
@@ -43,7 +43,7 @@ class Bird extends Animal {
 }
 
 public class TestReflect {
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchFieldException {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
 //        // 1. 如何获取类对象
 //        // 1). 通过字符串获取到类 参数是类的全限定类名
 //        //     通过字符串获取 可以是控制台输入、读取文件、网络获取
@@ -74,15 +74,34 @@ public class TestReflect {
 //        cat.eat("鱼");
 
         // 3. 通过反射获取/修改属性（可以修改 private 属性）
-        Class catClass = Class.forName("reflect.Cat");
+//        Class catClass = Class.forName("reflect.Cat");
         // Cat 类包含 name 属性 private
         // catClass 类对象是整个类的图纸 此处想关注图纸上一个局部信息
         // 通过 getDeclaredField 能得到局部内容 通过 Field 对象F表示
-        Field field = catClass.getDeclaredField("name");
+//        Field field = catClass.getDeclaredField("name");
+//        field.setAccessible(true);
+//
+//        Cat cat = new Cat();
+//        // 拿出图纸的局部信息 拆开看 cat 对象
+//        // 修改
+//        field.set(cat, "喵喵"); // cat.name = "喵喵"
+//        String name = (String) field.get(cat); // 相当于 cat.name
+//        System.out.println(name);
 
-        Cat cat = new Cat();
-        // 拿出图纸的局部信息 拆开看 cat 对象
-        String name = (String) field.get(cat); // 相当于 cat.name
-        System.out.println(name);
+//        // 4. 通过反射获取到类中的方法
+//        Class catClass = Class.forName("reflect.Cat");
+//        // 方法是可以重载 可以通过后续变长参数列表指定，要获取那个版本 eat
+//        // 下列代码意思 获取到只有一个参数 且类型为String 的eat 方法
+//        Method method = catClass.getMethod("eat", String.class);
+//
+//        Cat cat = new Cat();
+//        method.invoke(cat, "猫粮");
+
+        // 5. 获取构造方法 借助构造方法实例化
+        Class catClass = Class.forName("reflect.Cat");
+        // 获取到 Cat 类中 只有一个参数且类型为 String 的构造方法
+        Constructor constructor = catClass.getConstructor(String.class);
+        Cat cat = (Cat) constructor.newInstance("小黑");
+        cat.eat("鱼");
     }
 }
