5ab1cb47a5c9e885a4aaa245237da75e89eb39fe
s11235218 <s11235218@163.com>
2020-04-24 21:23:50 +0800

    数据结构

--- Collection/src/Stack/TestStack.java
+++ Collection/src/Stack/TestStack.java
@@ -7,9 +7,7 @@ import java.util.Stack;
 public class TestStack {
     public boolean isValid(String s) {
         Map<Character, Character> map = new HashMap<>();
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
+        
 
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
