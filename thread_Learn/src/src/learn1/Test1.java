3f89d04a161a382f169159b2673649bff0b76301
s11235218 <s11235218@163.com>
2020-05-15 22:22:21 +0800

    多线程

---
+++ thread_Learn/src/src/learn1/Test1.java
@@ -0,0 +1,57 @@
+package learn1;
+
+public class Test1 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(9999999999999L);
+
+        //
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(9999999999999L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, "练习1").start();
+
+        //
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(9999999999999L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, "练习1").start();
+        Thread.sleep(9999999999999L);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(9999999999999L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, "练习1");
+        t.run();
+
+        //
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("练习1");
+            }
+        }, "练习1").start();
+        System.out.println("main");
+
+    }
+}
