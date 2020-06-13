676054e08cb8321df04750b211f249519664ebf7
s11235218 <s11235218@163.com>
2020-06-13 19:11:07 +0800

    多线程、Maven创建、IO操作

--- MyThread/IO_Learn/src/PrintWriterTest.java
+++ MyThread/IO_Learn/src/PrintWriterTest.java
@@ -1,10 +1,22 @@
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class PrintWriterTest {
 
     @Test
     public void t1() {
-        Assert.assertTrue(true);
+        // Assert.assertTrue(true);
+        try {
+            int i = 1 / 0;
+            System.out.println("1/0");
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            System.out.println(sw.toString());
+        }
+        System.out.println("t1");
     }
 }
