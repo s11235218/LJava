cf8149bd6660eee200643136d3b0aaf00f3a30c7
s11235218 <s11235218@163.com>
2020-07-21 11:03:56 +0800

    多线程、Maven创建、IO操作、MyBlog

--- MyThread/TheadLearn/src/Thread2/JoinTest.java
+++ MyThread/TheadLearn/src/Thread2/JoinTest.java
@@ -1,8 +1,41 @@
 package Thread2;
 
+/**线程等待：
+ * （1） join
+ * （2) 结合activeCount() + yield() 使用
+ *         while (Thread.activeCount() > 1) {//用调试的方式运行
+ *             Thread.yield();//将当前线程由运行态-->就绪态
+ *         }
+ * */
+
 public class JoinTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void without() throws InterruptedException {
+        // 打印顺序 main--->thread-0
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        t.start();
+        t.join();
+        System.out.println(Thread.currentThread().getName());
+    }
+
+    public static void withoutSleep() throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        t.start();
+        t.join(2000);
+        System.out.println(Thread.currentThread().getName());
+    }
+
+    public static void withSleep() throws InterruptedException {
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -15,8 +48,18 @@ public class JoinTest {
             }
         });
         t.start();
-        // 等待线程死亡
         t.join(2000); // 调整参数 不传、1000、5000
         System.out.println("main");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 等待线程死亡
+        // 当前线程：代码执行的时候，所在的线程
+        // t线程：线程引用对象
+        // 当前线程进行阻塞（运行态-->阻塞态）等待（满足一定条件），t线程（不做任何处理，让t执行运行）
+        // 条件：（哪个条件先执行完，先满足）
+        //       a.传入时间（时间值+时间单位毫秒）
+        //       b.线程引用对象执行完毕
+
     }
 }
--- MyThread/TheadLearn/src/Thread2/ThreadYield.java
+++ MyThread/TheadLearn/src/Thread2/ThreadYield.java
@@ -8,8 +8,8 @@ public class ThreadYield {
                 System.out.println(Thread.currentThread().getName());
             }
         }).start();
-        // 等待new Thread所有线程执行完毕，否则一直等待
-        while (Thread.activeCount() > 1) {
+        // 等待new Thread所有线程执行完毕，否则一直等待 (线程让步)
+        while (Thread.activeCount() > 1) {//用调试的方式运行
             Thread.yield();//将当前线程由运行态-->就绪态
         }
         System.out.println(Thread.currentThread().getName());
