f5335ba8be2d6913060d0ae081eb125cb05c2d21
s11235218 <s11235218@163.com>
2020-09-16 19:38:51 +0800

    多线程、Maven创建、IO操作、MyBlog

--- MyThread/TheadLearn/src/Thread7/SequencePrint1.java
+++ MyThread/TheadLearn/src/Thread7/SequencePrint1.java
@@ -3,15 +3,28 @@ package Thread7;
 /*动态代码*/
 public class SequencePrint1 {
 
-    public static void print(final String[] array, final int count) {
-        int index = 0;
+    public static void print(String[] array, int count) {
+        Print.INDEX = 0;
         for (int i = 0; i < array.length; i++) {
             final int finalI = i;
-            new Thread(new Print()).start();
+            new Thread(new Print(array, count, finalI)).start();
         }
     }
 
     private static class Print implements Runnable {
+
+        private static volatile int INDEX;
+
+        private String[] array;
+        private int count;
+        private int finalI;
+
+
+        public Print(String[] array, int count, int finalI) {
+            this.array = array;
+            this.count = count;
+            this.finalI = finalI;
+        }
 
         @Override
         public void run() {
@@ -24,7 +37,7 @@ public class SequencePrint1 {
                         INDEX++;
                         System.out.print(array[finalI]);
                         if (finalI == array.length - 1){
-                            System.out.println("====" + j);
+                            System.out.println("=" + j);
                         }
                         SequencePrint.class.notifyAll();
                     }
@@ -40,7 +53,6 @@ public class SequencePrint1 {
         while (Thread.activeCount() > 1) {
             Thread.yield();
         }
-        print(new String[]{"A", "B", "C", "D"}, 10);
-
+        print(new String[]{"E", "B", "C", "D", "G", "F"}, 10);
     }
 }
