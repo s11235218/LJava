bcd7744ef8bc63c96b2d74f2972dc3ae4aac7854
s11235218 <s11235218@163.com>
2020-12-01 14:34:00 +0800

    Spring

--- JavaEEProject/lottery_draw/src/main/java/serein/Controller/UserController.java
+++ JavaEEProject/lottery_draw/src/main/java/serein/Controller/UserController.java
@@ -1,9 +1,12 @@
 package serein.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import serein.model.User;
 import serein.service.UserService;
+import sun.nio.cs.US_ASCII;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
     // 注意在类型上加@Service注解，注册到容器中，不然是空指针异常错误
     @Autowired
     private UserService userService;
@@ -22,6 +26,16 @@ public class UserController {
         HttpSession session = req.getSession();
         session.setAttribute("user", exist);
         return null;
-
     }
+
+    @PostMapping("/register")
+    public Object register(User user,
+                           // 上传的头像：1.保存在本地文件夹（web服务器需要加载到）
+                           // 2.url存放在注册用户的head字段
+                           @RequestPart(value = "headFile", required = false) MultipartFile headFile) {
+        userService.register(user, headFile);
+
+        return null;
+    }
+
 }
--- JavaEEProject/lottery_draw/src/main/java/serein/service/UserService.java
+++ JavaEEProject/lottery_draw/src/main/java/serein/service/UserService.java
@@ -1,16 +1,29 @@
 package serein.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import serein.exception.ClientException;
+import serein.exception.SystemException;
 import serein.mapper.UserMapper;
 import serein.model.User;
+
+import java.io.*;
 
 @Service
 public class UserService {
 
+    @Value("${user.head.local-path}")
+    private String localPath;
+    @Value("${user.head.remote-path}")
+    private String remotePath;
+    @Value("${user.head.filename}")
+    private String fileName;
+
     @Autowired
     private UserMapper userMapper;
+
     public User login(User user) {
 
 //        User exist = userMapper.login(user);
@@ -26,5 +39,33 @@ public class UserService {
         if (!exist.getPassword().equals(user.getPassword()))
             throw new ClientException("USR002", "用户名密码校验失败！");
         return exist;
+    }
+
+    public void register(User user, MultipartFile headFile) {
+
+        String path = "/" + user.getUsername() + "/" + fileName;
+        user.setHead(remotePath + path);
+        userMapper.insertSelective(user);
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+
+        try {
+            try {
+                File dir = new File(localPath + "/" + user.getUsername());
+                dir.mkdirs();
+                fos = new FileOutputStream(localPath + path);
+                bos = new BufferedOutputStream(fos);
+                bos.write(headFile.getBytes());
+                bos.flush();
+            } finally {
+                if (bos != null)
+                    bos.close();
+                if (fos != null)
+                    fos.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new SystemException("USER003", "用户注册失败，头像上传错误！");
+        }
     }
 }
