f3f5e9e44765a01c0f0c2322e5bce5b33e0c7f8c
s11235218 <s11235218@163.com>
2020-12-29 16:26:40 +0800

    学生宿舍管理系统

---
+++ MyProject/stu_dorm_sys/src/main/java/util/JSONUtil.java
@@ -0,0 +1,33 @@
+package util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+
+public class JSONUtil {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public static <T> T read(InputStream is, Class<T> tClass) {
+        try {
+            return MAPPER.readValue(is, tClass);
+        } catch (IOException e) {
+            throw new RuntimeException("JSON反序列化失败！", e);
+        }
+    }
+
+    public static String write(Object o) {
+        try {
+            return MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON", e);
+        }
+    }
+}
